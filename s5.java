1484ad3cfc155b74c049090a77b4a7614b5109fe
padam087 <pooja.jain.kashi@gmail.com>
2018-11-27 19:33:16 +0530

    stringIsEmpty

---
+++ s5.java
@@ -0,0 +1,14 @@
+//String IsEmpty() : This method checks whether the String contains anything or not.
+// If the java String is Empty, it returns true else false.  
+
+
+class s5
+{
+	public static void main(String args[])
+	{
+	String str="hello";
+	String str1="";
+	System.out.println(str.isEmpty());
+	System.out.println(str1.isEmpty());
+	}
+}
